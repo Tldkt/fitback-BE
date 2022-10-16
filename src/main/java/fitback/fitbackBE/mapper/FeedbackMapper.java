5cb2ca3bcf1bd5c396bd34ba9f4d8ea2f4e1e03f
Tldkt <evolym2543@sookmyung.ac.kr>
2022-10-16 17:59:15 +0900

    [chore] mapper 주석 양식 수정

--- src/main/java/fitback/fitbackBE/mapper/FeedbackMapper.java
+++ src/main/java/fitback/fitbackBE/mapper/FeedbackMapper.java
@@ -2,7 +2,10 @@ package fitback.fitbackBE.mapper;
 
 import fitback.fitbackBE.apicontroller.feedbackApiController;
 import fitback.fitbackBE.domain.Feedback;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel="spring")
+/*mapstruct의 인터페이스로 정의하기 위한 애너테이션, componentModel 애트리뷰트를 통해 bean등록*/
 public interface FeedbackMapper {
     Feedback mapCreateRqToFeedBack(feedbackApiController.CreateFeedbackRequest createFbrequest);
 
